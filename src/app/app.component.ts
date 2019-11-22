5fe454351c92bd30a4d9222f4342b9233b7feae5
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2019-11-22 14:07:48 +0100

    4 unique components were created. Posts and comments are displaying using directive *ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  arr = [1, 1, 3, 5, 8, 13];
+
+  objs = [
+    {title: 'Post 1', author: 'Natalia', comments: [
+        {name: 'Max', text: 'Lorem 1'},
+        {name: 'Max', text: 'Lorem 2'},
+        {name: 'Max', text: 'Lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Natalia 2', comments: [
+        {name: 'Max 2', text: 'Lorem 4'},
+        {name: 'Max 2', text: 'Lorem 5'},
+        {name: 'Max 2', text: 'Lorem 6'},
+      ]}
+  ];
+
 }
+
+
+
