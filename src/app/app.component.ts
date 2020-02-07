9dc309f866b71923a6c3fce6321b72b8dd7e355b
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2020-02-07 12:56:43 +0100

    RxJS Observable example; how create own stream. Lesson 167.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {interval, Subscription} from 'rxjs';
-import {map, filter} from 'rxjs/operators';
+import {Subscription, Observable} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,30 @@ export class AppComponent {
   sub: Subscription;
 
   constructor() {
-    const intervalStreams$ = interval(1000);
-    this.sub = intervalStreams$
-      .pipe(
-        filter( value => value % 2 === 0),
-        map((value) => `Mapped value ${value}`)
-      )
-      .subscribe((value) => {
-        console.log(value);
+    const stream$ = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1500);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 2100);
+
+      setTimeout( () => {
+        observer.error('Something went wrong!');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next(2);
+      }, 2500);
+
     });
+    this.sub = stream$
+      .subscribe(
+        value => console.log('Next: ', value),
+        error => console.log('Error: ', error),
+        () => console.log('Completed')
+      );
   }
 
   stop() {
